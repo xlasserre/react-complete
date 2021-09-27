b5e2ffbffe409ad8fb73a53f4088fea653ee6517
Ximena Lasserre <xglasserre@gmail.com>
2021-09-27 19:55:05 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage';
+import Header from './components/header';
 import Shop from './pages/shop';
 import Hats from './pages/hats';
 
@@ -9,6 +10,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={Shop} />
